06b8840e3ad83ec1f7924f8a06516c7f54e9c892
PRASHANTH <145742755+kannan-nagaraju@users.noreply.github.com>
2024-10-21 10:43:02 +0530

    Update JKFLIPFLOPUSINGIFELSE.v

--- JKFLIPFLOPUSINGIFELSE.v
+++ JKFLIPFLOPUSINGIFELSE.v
@@ -1,4 +1,4 @@
-module JKFLIPFLOPUSINGIFELSE(q, qb,j,k,clock,reset);
+module jkflipflop(q, qb,j,k,clock,reset);
     input j,k,clock,reset;
     output reg q, qb;
 	 
@@ -12,6 +12,22 @@ always @ (posedge (clock))
             end   
         
 else
- //Write logic for JK flipflop using if else statement for four conditions
-            
+         begin
+		 if (j==0 && k==0)
+		     begin
+		     q <= q;
+		     qb <= qb;
+		     end
+		 else if (j !=k)
+	              begin
+		      q <= j;
+		      qb <=k;
+		      end
+		 else if (j==1 && k==1)
+		      begin
+		      q <= ~q;
+		      qb = ~qb;
+		      end
+	 end
+    end
 endmodule
